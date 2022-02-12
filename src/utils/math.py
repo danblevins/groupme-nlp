8b81564c3212ffeb1eed750ed0ec2ef24f9f2ee5
Dan Blevins <37407464+danblevins@users.noreply.github.com>
2022-02-12 14:37:25 -0800

    Update math.py

--- src/utils/math.py
+++ src/utils/math.py
@@ -1,3 +1,12 @@
+import pandas as pd
+
+def analyze_time_series_data(data, column_name):
+    data = pd.DataFrame(data)
+    data.columns = [column_name]
+    data.reset_index(inplace=True)
+    data['month'] = data['send_time'].dt.month
+
+    return data
 
 def groupby_transform(data, groupby_column: str, transform_column: str):
     return data.groupby([groupby_column]).transform(transform_column)

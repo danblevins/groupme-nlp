05f442d3d4408d581d28c91f8200a19d3c1d1d30
Dan Blevins <37407464+danblevins@users.noreply.github.com>
2022-02-12 21:20:17 -0800

    Update run_nlp.py

--- src/activity/run_nlp.py
+++ src/activity/run_nlp.py
@@ -13,14 +13,6 @@ stop = STOP_WORDS
 nlp = English()
 tokenizer = nlp.tokenizer
 
-def analyze_time_series_data(data, column_name):
-    data = pd.DataFrame(data)
-    data.columns = [column_name]
-    data.reset_index(inplace=True)
-    data['month'] = data['send_time'].dt.month
-
-    return data
-
 def spacy_tokenizer_lemmatizer(text):
     tokens = tokenizer(text)
     

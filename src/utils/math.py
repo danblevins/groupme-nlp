9bce37331e3d8e3cf8d7649ee23eff6ba7ac474d
Dan Blevins <37407464+danblevins@users.noreply.github.com>
2022-02-12 21:20:43 -0800

    Update math.py

--- src/utils/math.py
+++ src/utils/math.py
@@ -7,6 +7,3 @@ def analyze_time_series_data(data, column_name):
     data['month'] = data['send_time'].dt.month
 
     return data
-
-def groupby_transform(data, groupby_column: str, transform_column: str):
-    return data.groupby([groupby_column]).transform(transform_column)

059de8cffcb212a46f8ab2583395c4bf01a6affe
Dan Blevins <37407464+danblevins@users.noreply.github.com>
2022-02-17 21:31:44 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,13 @@ def home():
 def superlatives():
     return render_template("superlatives.html")
 
+@app.route("/timeseries", methods=['GET','POST'])
+def time_series():
+    return render_template("time_series.html")
+
+@app.route("/textual", methods=['GET','POST'])
+def textual():
+    return render_template("textual.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
